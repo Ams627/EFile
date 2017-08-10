b71f9829551165a26c41a0a6501c3f748f632e8e
Ams <adriansims62781@gmail.com>
2017-08-10 23:15:57 +0100

    Add project files.

---
+++ EFile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFile
+{
+    internal class Program
+    {
+        static void GetFiles(ref List<string> files, string directory, string extensions)
+        {
+            var extensionsList = extensions.Split('|').ToList();
+            var filesInThisDir = new List<string>();
+            extensionsList.ForEach(x => filesInThisDir.AddRange(Directory.GetFiles(directory, "*." + x)));
+            var subdirs = Directory.GetDirectories(directory);
+            foreach (var dir in subdirs)
+            {
+                GetFiles(ref files, dir, extensions);
+            }
+            files.AddRange(filesInThisDir);
+        }
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var listOfFiles = new List<string>();
+                GetFiles(ref listOfFiles, @"Q:\classictvm", "c|h|cpp");
+                var filedic = listOfFiles.ToLookup(x => Path.GetFileName(x));
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add EFile/Program.cs
+git add EFile/App.config
+git add EFile/Properties/AssemblyInfo.cs
+git add EFile/EFile.csproj
+git add EFile.sln
+git add .gitignore
