b1ad94c05bdae6ddd07b26ec15d0491d872bd5bc
Adrian Sims <asims@parkeon.com>
2017-08-29 16:11:07 +0100

    Small changes

--- EFile/Program.cs
+++ EFile/Program.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace EFile
 {
     internal class Program
     {
+        const string indexFilename = ".fileIndex";
         static void GetFiles(ref List<string> files, string directory, string extensions)
         {
             var extensionsList = extensions.Split('|').ToList();
@@ -25,10 +27,57 @@ namespace EFile
         {
             try
             {
-                var listOfFiles = new List<string>();
-                GetFiles(ref listOfFiles, @"Q:\classictvm", "c|h|cpp");
-                var filedic = listOfFiles.ToLookup(x => Path.GetFileName(x));
-                Console.WriteLine();
+                if (args.Length == 1 && args[0] == "/index")
+                {
+                    var listOfFiles = new List<string>();
+                    var currentDir = Directory.GetCurrentDirectory();
+                    GetFiles(ref listOfFiles, currentDir, "c|h|cpp");
+                    var filedic = listOfFiles.ToLookup(x => Path.GetFileName(x));
+                    using (var streamWriter = new StreamWriter(indexFilename))
+                    {
+                        foreach (var groupEntry in filedic)
+                        {
+                            foreach (var filename in groupEntry)
+                            {
+                                streamWriter.WriteLine($"{groupEntry.Key}|{filename}");
+                            }
+                        }
+                    }
+                }
+                else if (args.Length > 0)
+                {
+                    var indexPath = GetIndexPath();
+                    if (indexPath != null)
+                    {
+                        var lines = File.ReadAllLines(indexPath);
+                        var lookup = lines.ToLookup(x => x.Split('|').First());
+                        foreach (var filename in args)
+                        {
+                            var results = lookup[filename];
+                            if (results.Count() > 1)
+                            {
+                                foreach (var matchedFilename in results)
+                                {
+                                    Console.WriteLine($"{matchedFilename}");
+                                }
+                            }
+                            else if (results.Count() == 0)
+                            {
+                                var pattern = "^" + filename;
+                                var matches = lookup.Where(x => Regex.Match(x.Key, pattern).Success);
+                                foreach (var matchgroup in matches)
+                                {
+                                    foreach (var match in matchgroup)
+                                    {
+                                        var matchedFilename = match.Split('|').Last();
+                                        Console.WriteLine($"{matchedFilename}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
             }
             catch (Exception ex)
             {
@@ -36,6 +85,25 @@ namespace EFile
                 var progname = Path.GetFileNameWithoutExtension(codeBase);
                 Console.Error.WriteLine(progname + ": Error: " + ex.Message);
             }
+        }
+
+        private static string GetIndexPath(string directory=".")
+        {
+            string result = "";
+            var pathname = Path.Combine(directory, indexFilename);
+            if (File.Exists(pathname))
+            {
+                result = pathname;
+            }
+            else
+            {
+                var parentInfo = Directory.GetParent(directory);
+                if (parentInfo != null)
+                {
+                    result = GetIndexPath(parentInfo.Name);
+                }
+            }
+            return result;
         }
     }
 }
